53a97adf64b3207fcfa1594d2f2440dabec1d2a4
Nikita Shilin <shilin.nick@yandex.ru>
2023-07-13 11:56:48 +0300

    restyled task card

--- src/components/taskCard.jsx
+++ src/components/taskCard.jsx
@@ -3,19 +3,11 @@ import { useState } from "react";
 const TaskCard = ({ id, task, status, sourceImage }) => {
   const [isCollapsed, setIsCollapsed] = useState(false);
   return (
-    <div className="taskCard">
+    <div className="taskCard" onClick={() => setIsCollapsed(!isCollapsed)}>
       <div className="taskCard__main">
         <div className="taskCard__id">{id}</div>
         <div className="taskCard__task">{task}</div>
         <div className="taskCard__status">{status}</div>
-
-        <button
-          onClick={() => {
-            setIsCollapsed(!isCollapsed);
-          }}
-        >
-          open
-        </button>
       </div>
       {isCollapsed && (
         <div className="taskCard__collapse">
