9712164feab0eded7957742aa0af2f411ba89eee
Nikita Shilin <shilin.nick@yandex.ru>
2023-07-13 11:02:44 +0300

    finished taskCard markup

--- src/components/taskCard.jsx
+++ src/components/taskCard.jsx
@@ -1,20 +1,33 @@
 import { useState } from "react";
 
-const TaskCard = ({ id, status }) => {
+const TaskCard = ({ id, task, status, sourceImage }) => {
   const [isCollapsed, setIsCollapsed] = useState(false);
   return (
     <div className="taskCard">
-      <div className="taskCard__id">{id}</div>
-      <div className="taskCard__status">{status}</div>
+      <div className="taskCard__main">
+        <div className="taskCard__id">{id}</div>
+        <div className="taskCard__task">{task}</div>
+        <div className="taskCard__status">{status}</div>
 
-      <button
-        onClick={() => {
-          setIsCollapsed(!isCollapsed);
-        }}
-      >
-        open
-      </button>
-      {isCollapsed && <div className="taskCard__collapse">'asd'</div>}
+        <button
+          onClick={() => {
+            setIsCollapsed(!isCollapsed);
+          }}
+        >
+          open
+        </button>
+      </div>
+      {isCollapsed && (
+        <div className="taskCard__collapse">
+          <div className="taskCard__previousWrapper">
+            <div className="taskCard__previousLabel">Source image</div>
+            <img className="taskCard__previousImage" alt="kek" src="img1.jpg" />
+          </div>
+          <button className="taskCard__downloadBTN">
+            DOWNLOAD PROCESSED IMAGE
+          </button>
+        </div>
+      )}
     </div>
   );
 };
--- src/pages/tasksPage.jsx
+++ src/pages/tasksPage.jsx
@@ -7,12 +7,12 @@ const TasksPage = () => {
     <>
       <div className="layout">
         <Table>
-          <TaskCard id={1} status={"processing"} />
-          <TaskCard id={2} status={"processing"} />
-          <TaskCard id={3} status={"processing"} />
-          <TaskCard id={4} status={"processing"} />
-          <TaskCard id={5} status={"processing"} />
-          <TaskCard id={6} status={"processing"} />
+          <TaskCard id={1} task={"resize"} status={"processing"} />
+          <TaskCard id={2} task={"resize"} status={"processing"} />
+          <TaskCard id={3} status={"processing"} task={"resize"} />
+          <TaskCard id={4} status={"processing"} task={"resize"} />
+          <TaskCard id={5} status={"processing"} task={"resize"} />
+          <TaskCard id={6} status={"processing"} task={"resize"} />
         </Table>
 
         <Navbar />
